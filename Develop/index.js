f258cd406f79b1d71ee877215d4717d8692df9fe
Stephen Lee <leestephen0320@gmail.com>
2024-10-08 05:57:51 -0700

    finished functionality for all query options. need to polish code later

--- Develop/index.js
+++ Develop/index.js
@@ -53,6 +53,53 @@ class Cli {
         }
     };
 
+    async addEmployee(firstName,lastName,role,manager) {
+        try {
+            const text1 = `SELECT id FROM roles WHERE title = '${role}'`;
+            const res1 = await pool.query(text1);
+
+            const text2 =  `SELECT id
+                FROM employee e
+                WHERE CONCAT(e.first_name,' ',e.last_name) = '${manager}'`;
+            const res2 = await pool.query(text2);
+
+            const text3 = `INSERT INTO employee (first_name,last_name,role_id,manager_id)
+                VALUES (${firstName},${lastName},${res1.rows[0]},${res2.rows[0]})`;
+            const res3 = await pool.query(text3);
+            console.table(res3.rows);
+        } catch (err) {
+            console.error('Error executing query',err.stack)
+        }
+     }
+
+     async updateEmployee(employeeName,role) {
+        try {
+            const text1 = `SELECT id FROM roles WHERE title = '${role}'`;
+            const res1 = await pool.query(text1);
+
+            const text3 = `UPDATE employee SET role_id = ${res1.rows[0]} 
+                WHERE CONCAT(e.first_name,' ',e.last_name) = '${employeeName}'`;
+            const res3 = await pool.query(text3);
+            console.table(res3.rows);
+        } catch (err) {
+            console.error('Error executing query',err.stack)
+        }
+     }
+
+     async getEmployees() {
+        try {
+            const text = `SELECT CONCAT(e.first_name,' ',e.last_name) AS employee_name FROM employee e`;
+            const res = await pool.query(text);
+            let employeess = [];
+            for (let i=0; i<res.rows.length; i++) {
+                employeess.push(res.rows[i].employee_name);
+            }
+            return employeess;
+        } catch (err) {
+            console.error('Error executing query',err.stack)
+        }
+     }
+
     async viewAllDepartments() {
         try {
             const text = `SELECT * FROM department`;
@@ -65,7 +112,7 @@ class Cli {
 
     async addDepartment(department) {
         try {
-            const text = `INSERT INTO department (name) VALUES ${department}`;
+            const text = `INSERT INTO department (name) VALUES '${department}'`;
             const res = await pool.query(text);
             console.table(res.rows);
         } catch (err) {
@@ -86,15 +133,6 @@ class Cli {
             console.error('Error executing query',err.stack)
         }
      }
-    
-    // async departmentFieldsReturn() {
-    //     departments = await this.getDepartment();
-    //     departmentFields = this.getDepartment().map(department => ({
-    //         department_name: department.department_name
-    //     }))
-    //     return this.departmentFields;
-    // }
-     
 
      async viewAllRoles() {
         try {
@@ -108,29 +146,45 @@ class Cli {
         }
      }
 
+     async getRoles() {
+        try {
+            const text = `SELECT * FROM roles`;
+            const res = await pool.query(text);
+            let roles = [];
+            for (let i=0; i<res.rows.length; i++) {
+                roles.push(res.rows[i].title);
+            }
+            return roles;
+        } catch (err) {
+            console.error('Error executing query',err.stack)
+        }
+     }
+
+     async getManagers() {
+        try {
+            const text = `SELECT CONCAT(e.first_name,' ',e.last_name) AS manager_name
+                FROM employee e
+                WHERE manager_id IS NULL`;
+            const res = await pool.query(text);
+            let managers = [];
+            for (let i=0; i<res.rows.length; i++) {
+                managers.push(res.rows[i].manager_name);
+            }
+            return managers;
+        } catch (err) {
+            console.error('Error executing query',err.stack)
+        }
+     }
+
      async addRole(title,salary,department) {
         try {
-            const text1 = `SELECT id FROM department WHERE department_name = ${department}`;
+            const text1 = `SELECT id FROM department WHERE department_name = '${department}'`;
             const res1 = await pool.query(text1);
 
             const text2 = `INSERT INTO roles (title,salary,department_id)
                 VALUES (${title},${salary},${res1.rows[0]}),`;
             const res2 = await pool.query(text2);
             console.table(res2.rows);
-        } catch (err) {
-            console.error('Error executing query',err.stack)
-        }
-     }
-
-     async getRole() {
-        try {
-            const text = `SELECT * FROM roles`;
-            const res = await pool.query(text);
-            let roles = [];
-            for (let i=0; i<res.rows.length; i++) {
-                roles.push(res.rows[i].role_name)
-            }
-            return roles;
         } catch (err) {
             console.error('Error executing query',err.stack)
         }
@@ -159,27 +213,84 @@ class Cli {
                 if (answers.action === 'View All Employees') {
                     this.viewAllEmployees();
                 } else if (answers.action === 'Add Employee') {
-                    inquirer
-                        .prompt([
-                            {
-                                type: 'input',
-                                name: 'first_name',
-                                message: `What is the employee's first name?`
-                            },
-                            {
-                                type: 'input',
-                                name: 'last_name',
-                                message: `What is the employee's last name?`
-                            },
-                            {
-                                type: 'list',
-                                name: 'role',
-                                message: `What is the employee's role?`,
-                                list: this.getRole()
-                            },
-                        ])
+                    return this.getRoles().then(roleChoices => {
+                        return this.getManagers().then(managerChoices => {
+                            return inquirer.prompt([
+                                {
+                                    type:'input',
+                                    name:'first_name',
+                                    message: `What is the employee's first name?`
+                                },
+                                {
+                                    type:'input',
+                                    name:'last_name',
+                                    message: `What is the employee's last name?`
+                                },
+                                {
+                                    type:'list',
+                                    name:'role',
+                                    message: `What is the employee's role?`,
+                                    choices: roleChoices
+                                },
+                                {
+                                    type:'list',
+                                    name:'manager',
+                                    message: `Who is the employee's manager?`,
+                                    choices: managerChoices
+                                }
+                            ]).then(employeeAnswers => {
+                                console.log(`Added ${employeeAnswers.first_name} ${employeeAnswers.last_name} to the database`)
+                                return this.addEmployee(employeeAnswers.firstName,employeeAnswers.lastName,employeeAnswers.role,employeeAnswers.manager);
+                            });
+                        })
+                    });
+                    // inquirer
+                    //     .prompt([
+                    //         {
+                    //             type: 'input',
+                    //             name: 'first_name',
+                    //             message: `What is the employee's first name?`
+                    //         },
+                    //         {
+                    //             type: 'input',
+                    //             name: 'last_name',
+                    //             message: `What is the employee's last name?`
+                    //         },
+                    //         {
+                    //             type: 'list',
+                    //             name: 'role',
+                    //             message: `What is the employee's role?`,
+                    //             list: this.getRole()
+                    //         },
+                    //         {
+                    //             type: 'list',
+                    //             name: 'manager',
+                    //             message: `Who is the employee's manager?`,
+                    //             list: this.getRole()
+                    //         },
+                    //     ])
                 } else if (answers.action === 'Update Employee Role') {
-                    
+                    return this.getEmployees().then(employeeChoices =>{
+                        return this.getRoles().then(roleChoices =>{
+                            return inquirer.prompt([
+                                {
+                                    type: 'list',
+                                    name: 'employeeName',
+                                    message: `Which employee's role do you want to update?`,
+                                    choices: employeeChoices,
+                                },
+                                {
+                                    type: 'list',
+                                    name: 'role',
+                                    message: 'Which role do you want to assign the selected employee?',
+                                    choices: roleChoices,
+                                },
+                            ]).then(updateAnswers => {
+                                console.log(`Updated ${updateAnswers.employeeName}'s role`);
+                                return this.updateEmployee(updateAnswers.employeeAnswers,updateAnswers.role);
+                            })
+                        })
+                    })
                 } else if (answers.action === 'View All Roles') {
                     this.viewAllRoles();
                 } else if (answers.action === 'Add Role') {
@@ -261,5 +372,6 @@ class Cli {
 
 const cli = new Cli;
 // cli.getDepartment().then()
-// console.log(departments);
-cli.performActions();
+const roles1 = await cli.getEmployees();
+console.log(roles1);
+//cli.performActions();
